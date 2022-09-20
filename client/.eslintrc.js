26c0daf260c22731037a423d988d783e0914e581
Miguel Barrenechea Sánchez <miguel.barrenechea@vizzuality.com>
2022-09-20 13:39:42 +0200

    Lefthook (#1)

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -12,6 +12,7 @@ module.exports = {
     },
     ecmaVersion: 12,
     sourceType: 'module',
+    tsconfigRootDir: __dirname,
     project: './tsconfig.eslint.json',
   },
   plugins: ['@typescript-eslint', 'prettier'],
