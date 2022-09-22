1db60adf39428ff1a1501dc1653a5244b2bdacd7
martintomas <martintomas.it@gmail.com>
2022-09-22 13:29:54 +0200

    feat: Sidekiq + redis

--- backend/config/environments/development.rb
+++ backend/config/environments/development.rb
@@ -62,4 +62,7 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  # Use a real queuing backend for Active Job (and separate queues per environment).
+  config.active_job.queue_adapter = :sidekiq
 end
