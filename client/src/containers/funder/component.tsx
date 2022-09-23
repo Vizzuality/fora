0bb9d09f1cae00a040ac399afd8ccb95cec2be36
Maria Sola <maria.sola@vizzuality.com>
2022-09-23 11:01:49 +0200

    refactor and general structure finished. Pending carousel and project page

--- client/src/containers/funder/component.tsx
+++ client/src/containers/funder/component.tsx
@@ -1,153 +1,16 @@
 import React from 'react';
 
-import cx from 'classnames';
-
-import Image from 'next/image';
-import Link from 'next/link';
-import { useRouter } from 'next/router';
-
-import { useFunder } from 'hooks/funders';
-
 import Wrapper from 'containers/wrapper';
 
-import Button from 'components/button';
-import Icon from 'components/icon';
-import Tooltip from 'components/tooltip';
-
-import CHEVRON_RIGHT_SVG from 'svgs/ui/chevron-right.svg?sprite';
-import INFO_SVG from 'svgs/ui/info.svg?sprite';
-
-import InfoCard from './info-card';
-
-const PROJECT_INFO = [
-  {
-    id: 'organization-type',
-    title: 'Organization Type',
-    info: 'EarthShare delivers the tools that businesses, individuals, and nonprofits need.',
-    subtitle: 'Public Foundation',
-  },
-  {
-    id: 'application-status',
-    title: 'Application Status',
-    info: 'EarthShare delivers the tools that businesses, individuals, and nonprofits need.',
-    subtitle: 'Invite only',
-  },
-  {
-    id: 'capital-acceptance',
-    title: 'Capital Acceptance',
-    info: 'EarthShare delivers the tools that businesses, individuals, and nonprofits need.',
-    subtitle: 'Donations Accepted',
-  },
-  {
-    id: 'capital-type',
-    title: 'Capital Type',
-    info: 'EarthShare delivers the tools that businesses, individuals, and nonprofits need.',
-    subtitle: 'Grant',
-  },
-];
+import FundersList from './list';
+import OverviewFunder from './overview';
 
 const Funder = () => {
-  const { query } = useRouter();
-  const { id: funderId } = query;
-  const { data: funderData } = useFunder(funderId);
-
   return (
     <Wrapper>
-      <div className="flex space-x-32">
-        <div className="flex-1 space-y-9">
-          <div>
-            <div className="text-base font-normal text-grey-20">Last updated: 30 March 2022</div>
-            <h2 className="text-3xl font-normal capitalize line-clamp-2 text-ellipsis">
-              {funderData.title}
-            </h2>
-          </div>
-
-          <div className="flex justify-between">
-            <div className="max-w-[50px]">
-              <Image
-                src="/images/avatar.png"
-                alt={funderData.title}
-                layout="responsive"
-                width={50}
-                height={50}
-              />
-            </div>
-            <Button type="button" size="xs" theme="black-alt">
-              Contact Funder
-            </Button>
-          </div>
-
-          <div className="space-y-2">
-            <p className="text-base font-semibold uppercase text-grey-20">Headquarters Adress</p>
-            <p>Washington, DC, United States</p>
-            <p className="font-semibold underline">
-              <a href="www.funderwebsite.org">www.funderwebsite.org</a>
-            </p>
-          </div>
-
-          <div className="py-4 border-y border-grey-40/40">
-            <dl className="grid grid-cols-2 grid-rows-2 gap-y-7">
-              {PROJECT_INFO.map(({ id, title, info, subtitle }) => (
-                <div key={id}>
-                  <span className="inline-flex items-center text-base font-semibold uppercase text-grey-20">
-                    <dt className="pr-2 uppercase whitespace-nowrap">{title}</dt>
-                    <Tooltip
-                      arrowProps={{
-                        enabled: true,
-                        size: 6,
-                        className: 'bg-grey-60',
-                      }}
-                      content={
-                        <div className="max-w-xs p-2.5 text-grey-20 rounded shadow-xl bg-grey-60 border border-grey-0/5">
-                          <span>{info}</span>
-                        </div>
-                      }
-                    >
-                      <div>
-                        <Icon
-                          icon={INFO_SVG}
-                          className={cx({
-                            'w-3 h-3 text-grey-20/30': true,
-                          })}
-                        />
-                      </div>
-                    </Tooltip>
-                  </span>
-                  <dd>{subtitle}</dd>
-                </div>
-              ))}
-            </dl>
-          </div>
-
-          <div className="space-y-3">
-            <p className="uppercase text-grey-20">About</p>
-            <p>
-              EarthShare delivers the tools that businesses, individuals, and nonprofits need to
-              maximize impact. Their all-in-one platform connects businesses, individuals, and
-              nonprofits with donation, volunteer, and carbon offsetting opportunities that directly
-              support today’s most important environmental efforts.
-            </p>
-          </div>
-        </div>
-        <div className="flex-1">
-          <InfoCard />
-        </div>
-      </div>
-      <div>
-        <div className="flex justify-between">
-          <h3 className="text-2xl font-display"> What is happening?</h3>
-          <div className="pt-4">
-            {/* <Link href="/"> */}
-            <a className="font-semibold underline decoration-1">View all projects</a>
-            <Icon
-              icon={CHEVRON_RIGHT_SVG}
-              className={cx({
-                'w-4 h-4 text-grey-0': true,
-              })}
-            />
-            {/* </Link> */}
-          </div>
-        </div>
+      <div className="pb-20 space-y-16">
+        <OverviewFunder />
+        <FundersList />
       </div>
     </Wrapper>
   );
--- client/src/containers/funder/list/component.tsx
+++ client/src/containers/funder/list/component.tsx
@@ -1,125 +1,71 @@
-import React, { useCallback } from 'react';
+import React from 'react';
 
 import cx from 'classnames';
 
-import { setSort } from 'store/funders';
-import { useAppDispatch, useAppSelector } from 'store/hooks';
+import Link from 'next/link';
 
-import { useFundersInfinity } from 'hooks/funders';
+import { useProjectsInfinity } from 'hooks/projects';
 
 import Cards from 'containers/cards';
-import Wrapper from 'containers/wrapper';
 
-import Button from 'components/button';
-import { Select } from 'components/forms';
-import { Option } from 'components/forms/select';
 import Icon from 'components/icon';
-import Loading from 'components/loading';
 
-import DOWNLOAD_SVG from 'svgs/ui/download.svg?sprite';
-import SHARE_SVG from 'svgs/ui/share.svg?sprite';
-
-const SORT_OPTIONS = [
-  { label: 'a-z', value: 'asc' },
-  { label: 'z-a', value: 'desc' },
-];
+import CHEVRON_RIGHT_SVG from 'svgs/ui/chevron-right.svg?sprite';
 
 const FundersList = () => {
-  const { filters, search, sort } = useAppSelector((state) => state['/funders']);
-  const dispatch = useAppDispatch();
-
-  const {
-    data: fundersData,
-    fetchNextPage: fetchNextPageFunders,
-    hasNextPage: hasNextFundersPage,
-    isFetchingNextPage: isFetchingNextFundersPage,
-  } = useFundersInfinity({
-    filters,
-    search,
-    sort,
-  });
-
-  const handleSortFunders = useCallback(
-    (value) => {
-      dispatch(setSort({ field: 'title', order: value }));
-    },
-    [dispatch]
-  );
-
-  const handleOnShowMore = useCallback(() => {
-    fetchNextPageFunders();
-  }, [fetchNextPageFunders]);
+  const { data: projectsData } = useProjectsInfinity();
 
   return (
-    <>
-      <Wrapper>
-        <div className="py-8">
-          <p className="max-w-md line-clamp-2">
-            Your are viewing <strong>{fundersData.length} funders</strong> from{' '}
-            <strong>All U.S regions</strong> who invest in{' '}
-            <strong>Toxins Reduction, Food Sovereignity, Climate Change</strong>
-          </p>
-
-          <div className="flex justify-between">
-            <div className="flex items-center w-1/12">
-              <Select
-                placeholder="Sort by"
-                theme="light"
-                size="sm"
-                render={(selected) => <div>{selected.label}</div>}
-                value={sort.order}
-                onChange={handleSortFunders}
-              >
-                {SORT_OPTIONS.map(({ label, value }) => (
-                  <Option key={value} value={value} theme="light" label={label}>
-                    {label}
-                  </Option>
-                ))}
-              </Select>
-            </div>
-
-            <div className="flex m-3 border divide-x rounded-md divide-solid border-grey-20">
-              <button type="button" className="px-3 py-3">
+    <div className="space-y-20">
+      <div className="space-y-9">
+        <div className="flex items-center justify-between">
+          <h3 className="text-2xl font-display"> What is happening?</h3>
+          <div>
+            <Link href="/projects">
+              <a className="flex items-center space-x-3 font-semibold underline decoration-1">
+                <span>View all projects</span>
                 <Icon
-                  icon={SHARE_SVG}
+                  icon={CHEVRON_RIGHT_SVG}
                   className={cx({
-                    'w-4 h-4 text-grey-0': true,
+                    'w-2 h-2 text-grey-0': true,
                   })}
                 />
-              </button>
-              <button type="button" className="px-3 py-3">
+              </a>
+            </Link>
+          </div>
+        </div>
+        <Cards data={projectsData} />
+      </div>
+
+      <div className="space-y-9">
+        <div className="flex items-center justify-between">
+          <h3 className="text-2xl font-display"> What are the similar funders?</h3>
+          <div>
+            <Link href="/funders">
+              <a className="flex items-center space-x-3 font-semibold underline decoration-1">
+                <span>View all funders</span>
                 <Icon
-                  icon={DOWNLOAD_SVG}
+                  icon={CHEVRON_RIGHT_SVG}
                   className={cx({
-                    'w-4 h-4 text-grey-0': true,
+                    'w-2 h-2 text-grey-0': true,
                   })}
                 />
-              </button>
-            </div>
+              </a>
+            </Link>
           </div>
         </div>
 
-        <Cards data={fundersData} />
+        <div className="space-y-3">
+          <div className="font-semibold capitalize text-grey-20">By Geographic Scope</div>
+          <Cards data={projectsData} />
+        </div>
 
-        {hasNextFundersPage && (
-          <div className="flex justify-center py-10">
-            <Button
-              type="button"
-              theme="black-alt"
-              size="xl"
-              className="overflow-hidden"
-              onClick={handleOnShowMore}
-            >
-              Show more
-              <Loading
-                visible={isFetchingNextFundersPage}
-                className="absolute flex items-center justify-center w-full h-full bg-white"
-              />
-            </Button>
-          </div>
-        )}
-      </Wrapper>
-    </>
+        <div className="space-y-3">
+          <div className="font-semibold capitalize text-grey-20">By Area of Focus</div>
+          <Cards data={projectsData} />
+        </div>
+      </div>
+    </div>
   );
 };
 
