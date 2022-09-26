04b11ba1ce3eb2ddbdb2a9050e258f23439e2f2f
Miguel Barrenechea Sánchez <miguel.barrenechea@vizzuality.com>
2022-09-26 12:34:00 +0200

    Funders & Projects: getStaticPaths & getStaticProps fallback false

--- client/src/pages/projects/[id].tsx
+++ client/src/pages/projects/[id].tsx
@@ -36,7 +36,7 @@ export async function getStaticPaths() {
 
   return {
     paths,
-    fallback: 'blocking', // can also be true or 'blocking'
+    fallback: false, // can also be true or 'blocking'
   };
 }
 
