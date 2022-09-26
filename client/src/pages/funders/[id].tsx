7a1478eb5c22ecc54a8daac0d806f9009f4fcf0f
Miguel Barrenechea Sánchez <miguel.barrenechea@vizzuality.com>
2022-09-26 12:18:34 +0200

    Funders & Projects: getStaticPaths & getStaticProps

--- client/src/pages/funders/[id].tsx
+++ client/src/pages/funders/[id].tsx
@@ -1,6 +1,10 @@
 import Head from 'next/head';
 
+import { dehydrate, QueryClient } from '@tanstack/react-query';
+
 import MetaTags from 'containers/meta-tags';
+
+import FUNDERS from 'services/funders';
 
 const DESCRIPTION_TEXT =
   'FORA (Funders for Regenerative Agriculture) is a network of funders and funder initiatives aimed at informing, educating, organizing, providing collaborative opportunities, and recruiting new members in support of regenerative agricultural systems. ';
@@ -18,4 +22,42 @@ const FundersDetailPage: React.FC = () => {
   );
 };
 
+export async function getStaticPaths() {
+  const response = await FUNDERS.request({
+    method: 'GET',
+    url: '/',
+  }).then((r) => r.data);
+
+  const paths = response.map((funder) => ({
+    params: { id: `${funder.id}` },
+  }));
+
+  // console.log('paths', paths);
+
+  return {
+    paths,
+    fallback: 'blocking', // can also be true or 'blocking'
+  };
+}
+
+export async function getStaticProps(ctx) {
+  const { id } = ctx.params;
+  const queryClient = new QueryClient();
+
+  await queryClient.prefetchQuery(['projects', id], () => {
+    return FUNDERS.request({
+      method: 'GET',
+      url: `/${id}`,
+    }).then((r) => r.data);
+  });
+
+  // console.log(queryClient.getQueriesData(['projects', id]));
+
+  // Props returned will be passed to the page component
+  return {
+    props: {
+      dehydratedState: dehydrate(queryClient),
+    },
+  };
+}
 export default FundersDetailPage;
--- client/src/pages/projects/[id].tsx
+++ client/src/pages/projects/[id].tsx
@@ -1,6 +1,10 @@
 import Head from 'next/head';
 
+import { dehydrate, QueryClient } from '@tanstack/react-query';
+
 import MetaTags from 'containers/meta-tags';
+
+import PROJECTS from 'services/projects';
 
 const DESCRIPTION_TEXT =
   'FORA (Funders for Regenerative Agriculture) is a network of funders and funder initiatives aimed at informing, educating, organizing, providing collaborative opportunities, and recruiting new members in support of regenerative agricultural systems. ';
@@ -17,5 +21,44 @@ const ProjectsDetailPage: React.FC = () => {
     </div>
   );
 };
+
+export async function getStaticPaths() {
+  const response = await PROJECTS.request({
+    method: 'GET',
+    url: '/',
+  }).then((r) => r.data);
+
+  const paths = response.map((funder) => ({
+    params: { id: `${funder.id}` },
+  }));
+
+  // console.log('paths', paths);
+
+  return {
+    paths,
+    fallback: 'blocking', // can also be true or 'blocking'
+  };
+}
+
+export async function getStaticProps(ctx) {
+  const { id } = ctx.params;
+  const queryClient = new QueryClient();
+
+  await queryClient.prefetchQuery(['projects', id], () => {
+    return PROJECTS.request({
+      method: 'GET',
+      url: `/${id}`,
+    }).then((r) => r.data);
+  });
+
+  // console.log(queryClient.getQueriesData(['projects', id]));
+
+  // Props returned will be passed to the page component
+  return {
+    props: {
+      dehydratedState: dehydrate(queryClient),
+    },
+  };
+}
 
 export default ProjectsDetailPage;
