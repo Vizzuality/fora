b997b73f0dcbe2efabb7698308c38b97eb1c54ed
Maria Sola <maria.sola@vizzuality.com>
2022-10-03 12:57:22 +0200

    small update on screen h and w

--- client/src/pages/404.tsx
+++ client/src/pages/404.tsx
@@ -8,7 +8,7 @@ const Page404: React.FC = () => {
   const handleGoBack = useCallback(() => Router.back(), []);
 
   return (
-    <div className="flex flex-col items-center justify-center h-full py-10 space-y-11">
+    <div className="flex flex-col items-center justify-center w-screen h-screen space-y-11">
       <div className="py-20">
         <h2 className="text-green-0 text-[200px] font-display font-normal">404</h2>
       </div>
