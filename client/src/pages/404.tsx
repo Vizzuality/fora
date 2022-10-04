99de605f471ec420a0246e683fe8c7625dddde26
Maria Sola <maria.sola@vizzuality.com>
2022-10-04 09:51:12 +0200

    fix in styles

--- client/src/pages/404.tsx
+++ client/src/pages/404.tsx
@@ -1,14 +1,10 @@
-import React, { useCallback } from 'react';
-
-import Router from 'next/router';
+import React from 'react';
 
 import Button from 'components/button';
 
 const Page404: React.FC = () => {
-  const handleGoBack = useCallback(() => Router.back(), []);
-
   return (
-    <div className="flex flex-col items-center justify-center w-screen grow space-y-11">
+    <div className="flex flex-col items-center justify-center w-screen py-20 grow space-y-11">
       <div className="py-20">
         <h2 className="text-green-0 text-[200px] font-display font-normal">404</h2>
       </div>
