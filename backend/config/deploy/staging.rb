0911202c1217df69cf98dc173d948286fb131d96
Agnieszka Figiel <agnieszka.figiel@gmail.com>
2022-10-10 16:14:48 +0200

    Revert "Set all possible envs"
    This reverts commit 4695dfbe9f3f67bf8f1cca5db2daeb4bb511c7be.

--- backend/config/deploy/staging.rb
+++ backend/config/deploy/staging.rb
@@ -1,7 +1,2 @@
 server "52.1.158.47", user: "ubuntu", roles: %w[web app db]
 set :branch, "staging"
-
-set :rails_env, :production
-set :rack_env, :production
-set :stage, :staging
-set :appsignal_env, :staging
