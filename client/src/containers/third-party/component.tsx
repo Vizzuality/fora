1e173a3c8a53589ee1d04ee53e5d41e4c917d371
Maria Sola <maria.sola@vizzuality.com>
2022-11-14 17:05:37 +0100

    cookies erased

--- client/src/containers/third-party/component.tsx
+++ client/src/containers/third-party/component.tsx
@@ -6,58 +6,24 @@ import Script from 'next/script';
 
 import { GA_TRACKING_ID } from 'lib/analytics/ga';
 
-import { useModal } from 'hooks/modals';
-
-import Cookies from 'components/cookies';
-
 const ThirdParty: React.FC = () => {
-  const [consentCookie, setConsentCookie] = useCookie('consent', undefined);
-
-  const consent = useMemo(() => {
-    if (consentCookie === 'true') return true;
-    if (consentCookie === 'false') return false;
-    return undefined;
-  }, [consentCookie]);
-
-  const { isOpen: isOpenCookies, open: openCookies, close: closeCookies } = useModal();
-
-  const handleCookieClick = (c) => {
-    setConsentCookie(String(c));
-    closeCookies();
-  };
-
-  useEffect(() => {
-    if (!consentCookie) {
-      openCookies();
-    }
-  }, [consentCookie, openCookies]);
-
   return (
     <>
-      {consent && (
-        <>
-          {/* Third Party Script needing cookies */}
-          {/* Global site tag (gtag.js) - Google Analytics */}
-          <Script
-            id="gtm-script"
-            strategy="afterInteractive"
-            src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
-          />
-          <Script id="gtm-config-script" strategy="afterInteractive">
-            {`
+      {/* Third Party Script needing cookies */}
+      {/* Global site tag (gtag.js) - Google Analytics */}
+      <Script
+        id="gtm-script"
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+      />
+      <Script id="gtm-config-script" strategy="afterInteractive">
+        {`
               window.dataLayer = window.dataLayer || [];
               function gtag(){dataLayer.push(arguments);}
               gtag('js', new Date());
               gtag('config', '${GA_TRACKING_ID}');
             `}
-          </Script>
-        </>
-      )}
-      <Cookies
-        open={isOpenCookies}
-        onAccept={() => handleCookieClick(true)}
-        onReject={() => handleCookieClick(false)}
-      />
+      </Script>
     </>
   );
 };
