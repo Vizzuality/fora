c26e33288a8deae0f485b701a9b3e148e47bb260
Miguel Barrenechea Sánchez <miguel.barrenechea@vizzuality.com>
2022-11-17 15:33:07 +0100

    Dashboards: meta (#71)

--- client/src/containers/dashboards/general-report/totals/charts/component.tsx
+++ client/src/containers/dashboards/general-report/totals/charts/component.tsx
@@ -17,6 +17,36 @@ const SLUGS = [
   'total_projects_recipient_legal_statuses',
 ];
 
+const META = {
+  total_projects_capital_types: {
+    description:
+      'Sum of all projects filtered by capital type and year of investment (==year of report)',
+  },
+  total_projects_demographics: {
+    description:
+      'Sum of all projects filtered by demographic focus and year of investment (==year of report)',
+  },
+  total_funders_capital_types: {
+    description:
+      'Sum of all funders filtered by capital type and year of investment (==year of report)',
+  },
+  total_funders_demographics: {
+    description:
+      'Sum of all funders filtered by demographic focus and year of investment (==year of report)',
+  },
+  total_funders_funder_types: {
+    description:
+      'Sum of all funders filtered by organization type and year of investment (==year of report)',
+  },
+  total_funders_capital_acceptances: {
+    description:
+      'Sum of all funders filtered by capital acceptance and year of investment (==year of report)',
+  },
+  total_projects_recipient_legal_statuses: {
+    description:
+      'Sum of all projects filtered by project legal status and year of investment (==year of report)',
+  },
+};
 const ReportTotalsCharts = () => {
   const { filters } = useAppSelector((state) => state['/dashboards/general-report']);
 
@@ -37,6 +67,7 @@ const ReportTotalsCharts = () => {
           <div key={widget.id} className="col-span-12 md:col-span-6">
             <Widget
               {...widget}
+              description={META[widget.slug]?.description}
               config={{ type: 'pie', className: 'bg-grey-60' }}
               params={{ filters }}
             />
