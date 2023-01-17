3b2a75a2e8d0c4c332dc42661edf5aa18b555b4d
Miguel Barrenechea Sánchez <miguel.barrenechea@vizzuality.com>
2023-01-17 13:22:44 +0100

    General report: multiple years. Loaders. No data for diagrams (#90)

--- client/src/types/dashboards.ts
+++ client/src/types/dashboards.ts
@@ -1,2 +1,8 @@
 export type ReportPages = 'general_report' | 'disparities_report' | 'interactive_report';
-export type ReportYears = 2021;
+export type ReportYears = 2021 | 2022 | 2023;
+
+export type ReportYear = {
+  id: string;
+  name: ReportYears;
+  type: 'report_year';
+};
