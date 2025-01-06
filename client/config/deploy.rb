fe52fcc7383b71bb7eecd0e7f4faeb0b1da5bbe8
martintomas <martintomas.it@gmail.com>
2025-01-06 11:13:30 +0100

    chore: Updating node version at deploy script

--- client/config/deploy.rb
+++ client/config/deploy.rb
@@ -6,9 +6,9 @@ set :repo_url, 'https://github.com/Vizzuality/fora.git'
 set :repo_tree, 'client'
 set :deploy_to, '/var/www/fora-client'
 set :nvm_type, :user
-set :nvm_node, 'v16.14.2'
-set :nvm_map_bins, %w{node npm yarn}
-set :yarn_flags, %w{--silent}
+set :nvm_node, 'v22.11.0'
+set :nvm_map_bins, %w{node npm pnpm}
+set :pnpm_flags, %w{--silent}
 
 set :repository_cache, "git_cache"
 set :deploy_via, :remote_cache
@@ -23,14 +23,14 @@ set :passenger_roles, :web
 append :linked_files, '.env.local'
 append :linked_dirs, 'node_modules'
 
-namespace :yarn do
-  after 'yarn:install', 'yarn:build'
+namespace :pnpm do
+  after 'pnpm:install', 'pnpm:build'
 
   task :build do
-    on roles fetch(:yarn_roles) do
-      within fetch(:yarn_target_path, release_path) do
-        with fetch(:yarn_env_variables, {}) do
-          execute :yarn, 'build'
+    on roles fetch(:pnpm_roles) do
+      within fetch(:pnpm_target_path, release_path) do
+        with fetch(:pnpm_env_variables, {}) do
+          execute :pnpm, 'build'
         end
       end
     end
