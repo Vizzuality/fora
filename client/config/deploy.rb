7269b7a214c88079d6a3dff6c37960cb3252c2f9
martintomas <martintomas.it@gmail.com>
2025-01-06 12:07:04 +0100

    refactoring: Making node modules as not shared directory

--- client/config/deploy.rb
+++ client/config/deploy.rb
@@ -8,12 +8,13 @@ set :deploy_to, '/var/www/fora-client'
 set :nvm_type, :user
 set :nvm_node, 'v22.11.0'
 set :nvm_map_bins, %w{node npm pnpm}
-set :pnpm_flags, %w{--silent} + ["--store=#{shared_path}/node_modules"]
+set :pnpm_flags, %w{--silent}
 
 set :repository_cache, "git_cache"
 set :deploy_via, :remote_cache
 
 set :keep_releases, 3
+set :keep_assets, 3
 
 set :init_system, :systemd
 
@@ -21,7 +22,6 @@ set :passenger_restart_with_sudo, true
 set :passenger_roles, :web
 
 append :linked_files, '.env.local'
-append :linked_dirs, 'node_modules'
 
 namespace :pnpm do
   after 'pnpm:install', 'pnpm:build'
