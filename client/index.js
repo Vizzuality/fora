e37ecb1c0db2e4cb3664fec808c7ff9ca430bdd4
martintomas <martintomas.it@gmail.com>
2025-01-07 14:32:06 +0100

    fix: Run next executable instead of next bash script

--- client/index.js
+++ client/index.js
@@ -1,6 +1,6 @@
 const path = require('path');
 
-const nextPath = path.join(__dirname, 'node_modules', '.bin', 'next');
+const nextPath = path.join(__dirname, 'node_modules', 'next/dist/bin/next');
 
 process.argv.length = 1;
 process.argv.push(nextPath, 'start');
